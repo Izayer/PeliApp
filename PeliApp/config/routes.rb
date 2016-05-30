0c32de665554f186af67a02f78b9bc758bda944a
Iker <iker_izagirre@hotmail.com>
2016-05-30 06:10:04 +0000

    Sin que salgan las peliculas

--- PeliApp/config/routes.rb
+++ PeliApp/config/routes.rb
@@ -53,4 +53,11 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  
+ 
+	get '/movies' => 'movies#index'
+	get '/movies/:id' => 'movies#show', as: :movie
+	get '/actors' => 'actors#index'
+	get '/actors/:id' => 'actors#show', as: :actor
+    
 end
